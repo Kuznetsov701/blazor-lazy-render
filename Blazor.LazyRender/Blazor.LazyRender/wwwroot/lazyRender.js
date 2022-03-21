cd38c48431aaedfa9a7301c363b809b7f8e9497a
Kuznetsov701 <kuznetcov701@gmail.com>
2022-03-22 01:10:17 +0700

    Create

---
+++ Blazor.LazyRender/Blazor.LazyRender/wwwroot/lazyRender.js
@@ -0,0 +1,13 @@
+﻿export function observe(element, dotnetReference) {
+	var observer = new IntersectionObserver(function (entries) {
+		if (entries[0]['isIntersecting'] === true) {
+			observer.disconnect();
+			dotnetReference.invokeMethodAsync('Intersected');
+		}
+	});
+	observer.observe(element);
+	return observer;
+}
+export function unobserve(observer) {
+	observer.disconnect();
+}
